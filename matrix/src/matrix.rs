819191c1ba7e2dad595451bcf125eb417114c2b7
Jason Guo <sg763@cornell.edu>
2022-06-19 22:15:19 -0400

    Get ready for gpu

--- matrix/src/matrix.rs
+++ matrix/src/matrix.rs
@@ -1,13 +1,15 @@
 use std::{cmp, fmt, ops};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct MatrixError;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Matrix<T>
 where
     T: fmt::Display,
 {
+    // `data` is a matrix with `rsize` rows and `csize` columns
+    // RI: `csize` > 0, `rsize` > 0,`data.len() == rsize*csize`
     rsize: usize,
     csize: usize,
     data: Vec<T>,
@@ -20,7 +22,7 @@ where
     /// Constructs a new `Matrix` Result with `rsize` rows, `csize` columns, and elements `data`.
     /// Requires: dimensions match the length of `data`
     pub fn new(rsize: usize, csize: usize, data: Vec<T>) -> Result<Matrix<T>, MatrixError> {
-        if rsize * csize != data.len() {
+        if !(rsize > 0 && csize > 0 && rsize * csize == data.len()) {
             Err(MatrixError)
         } else {
             Ok(Matrix { rsize, csize, data })
@@ -34,9 +36,9 @@ where
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut ret = String::new();
-        for row in 0..cmp::min(10, self.rsize) {
-            for col in 0..cmp::min(10, self.csize) {
-                ret.push_str(&format!("{} ", self.data[row * self.csize + col]));
+        for r in 0..cmp::min(10, self.rsize) {
+            for c in 0..cmp::min(10, self.csize) {
+                ret.push_str(&format!("{} ", self.data[r * self.csize + c]));
             }
             ret.push_str("\n");
         }
